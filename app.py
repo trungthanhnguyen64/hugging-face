c12f241a18392a1bb80ee24c117cd8bc1391ab83
titvvn <trungnguyenthanh1901@gmail.com>
2025-07-21 21:58:21 +0700

    Add

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from transformers import pipeline
-import torch
 import gradio as gr
 
-
-model = pipeline(
-    "summarization",
-)
+model = pipeline("summarization")
 
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, outputs="text")
 
-# create an interface for the model
-with gr.Interface(predict, "textbox", "text") as interface:
-    interface.launch()
+demo.launch()
